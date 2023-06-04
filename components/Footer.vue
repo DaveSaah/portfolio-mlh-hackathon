36422cf6ec9cb106c44d1aab51b2826b7b7b3126
David Saah <dasorange.hope@gmail.com>
2023-06-04 11:05:25 +0000

    add footer component

---
+++ components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="text-white text-center py-6">
+    <hr
+      class="my-12 h-1 border-0 bg-transparent bg-gradient-to-r from-transparent via-sky-950 to-transparent"
+    />
+    &copy 2023 DaveSaah <br />
+    Powered by Nuxt <Icon name="logos:nuxt-icon" /> + Vue <Icon name="logos:vue" /> + Tailwind <Icon name="logos:tailwindcss-icon" />
+  </div>
+</template>
