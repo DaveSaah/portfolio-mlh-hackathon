07912d25e6e55f5443bda3a1615fb5c8cdf3f55e
David Saah <dasorange.hope@gmail.com>
2023-06-04 11:04:58 +0000

    add nav links component

---
+++ components/NavLinks.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+const navlinks = ref([
+  { id: "#home", label: "Home", clicked: true },
+  { id: "#about", label: "About", clicked: false },
+  { id: "#projects", label: "Projects", clicked: false },
+  { id: "#explore", label: "Explorations", clicked: false },
+]);
+
+function toggle(navLabel: string) {
+  navlinks.value.forEach((element) => {
+    element.label === navLabel
+      ? (element.clicked = true)
+      : (element.clicked = false);
+  });
+}
+</script>
+
+<template>
+  <ul class="text-sm leading-6">
+    <li>
+      <a
+        v-for="nav in navlinks"
+        :href="nav.id"
+        @click="
+          toggle(nav.label);
+          console.log(nav.clicked);
+        "
+        :class="[
+          nav.clicked ? 'text-sky-500' : '',
+          'block py-1 font-semibold hover:text-green-400',
+        ]"
+      >
+        {{ nav.label }}
+      </a>
+    </li>
+  </ul>
+</template>
