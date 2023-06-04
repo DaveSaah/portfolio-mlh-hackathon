7e037e2d284e658ed8be5c156a73db20334f52f0
David Saah <dasorange.hope@gmail.com>
2023-06-04 11:06:08 +0000

    Add home section component

---
+++ components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="p-5 flex flex-row justify-center justify items-center">
+    <div class="flex flex-col grow">
+      <h1 class="font-mono text-5xl tracking-wide">
+        I'm<br />
+        <span class="font-semibold text-8xl">David Saah</span>
+      </h1>
+      <p>
+        <img
+          src="https://readme-typing-svg.herokuapp.com?font=Fira+Code&color=00ffd2&size=20&duration=5000&center=false&width=900&vCenter=true&lines=I+am+a+curious+learner!;I+love+to+solve+problems+using+code.;My+advice+is:+Take+small+steps,+leave+no+gaps,+everything+makes+sense."
+        />
+      </p>
+    </div>
+    <div>
+      <img
+        src="/images/computer.svg"
+        width="500"
+        class="object-contain rounded"
+        alt="computer"
+      />
+    </div>
+  </div>
+</template>
