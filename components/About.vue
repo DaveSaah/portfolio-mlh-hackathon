b6a88dfbda07295c51d750641daf8ded62f99fcb
David Saah <dasorange.hope@gmail.com>
2023-06-14 16:35:28 +0000

    change from generic card to about card

--- components/About.vue
+++ components/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="py-8">
     <h1 class="text-4xl mb-6">About Me</h1>
-    <Card
+    <AboutCard
       title="Explorer ~ Opensource"
       body="Linux made me like this. I have built many projects these past few years, to learn and solve my problems. The next challenge is to use my skills to solve the problems of others as well. In addition, I write technical articles about opensource technology and programming languages."
       img-src="/images/opensource.png"
@@ -11,9 +11,9 @@
       ]"
       timeline="2019 - present"
     />
-    <Card
+    <AboutCard
       title="Code Review Shop"
-      body="CodeReviewShop is my medium of giving back to the community. I aim to help students grow with a solid foundation of the concepts and tools we use in the industry. Since its inception, I have guided 20+ students through programming projects and lessons. I also started CodeReview Blog to share lessons about getting started with programming."
+      body="My medium of giving back to the community. I aim to help students grow with a solid foundation of the concepts and tools we use in the industry. Since its inception, I have guided 20+ students through programming projects and lessons. I also started CodeReview Blog to share lessons about getting started with programming."
       img-src="/images/codereview-logo.png"
       :links="[
         { link: 'https://codereviewshop.web.app', label: 'CodeReviewShop' },
@@ -22,7 +22,7 @@
       timeline="2023 - present"
     />
 
-    <Card
+    <AboutCard
       title="Student ~ Ashesi University"
       body="I am a sophomore majoring in Computer Science. I am also a Mastercard Foundation Scholar. Mastercard Foundation scholarraship package is highly competitive and awarded to students who are academically strong and have demonstrated a commitment to making a difference in the lives of others."
       timeline="2021 - 2025"
---
+++ components/AboutCard.vue
@@ -0,0 +1,35 @@
+<script setup>
+defineProps({
+  title: String,
+  imgSrc: String,
+  body: String,
+  timeline: String,
+  links: Array,
+});
+</script>
+
+<template>
+  <div class="flex flex-row rounded-lg bg-slate-800 w-full h-56 my-4">
+    <img
+      class="h-70 w-70 rounded-t-lg rounded-b-lg object-cover"
+      :src="imgSrc"
+      :alt="title"
+    />
+    <div class="flex flex-col justify-start p-6">
+      <h5 class="mb-2 text-xl font-semibold text-neutral-50">
+        {{ title }}
+      </h5>
+      <p class="mb-4 text-base text-neutral-200">
+        {{ body }}
+      </p>
+      <div v-for="item in links" class="flex flex-col items-left">
+        <a :href="item.link" class="text-sky-500" target="_blank">
+          <Icon name="icon-park-outline:link-one" /> {{ item.label }}
+        </a>
+      </div>
+      <p class="text-xs text-neutral-300 pt-4">
+        {{ timeline }}
+      </p>
+    </div>
+  </div>
+</template>
