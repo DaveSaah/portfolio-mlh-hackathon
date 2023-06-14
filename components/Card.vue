1b64e2a4c15669240b93acf308c174f9d1722c39
David Saah <dasorange.hope@gmail.com>
2023-06-14 16:34:29 +0000

    make project card generic to make it reusable

--- components/Card.vue
+++ components/Card.vue
@@ -1,35 +1,39 @@
 <script setup>
 defineProps({
   title: String,
-  imgSrc: String,
   body: String,
-  timeline: String,
-  links: Array,
+  link: String,
+  color: String,
+  lang: String,
+  icon: String,
 });
 </script>
 
 <template>
-  <div class="flex flex-row rounded-lg bg-slate-800 w-full h-56 my-4">
-    <img
-      class="h-70 w-70 rounded-t-lg rounded-b-lg object-cover"
-      :src="imgSrc"
-      :alt="title"
-    />
-    <div class="flex flex-col justify-start p-6">
-      <h5 class="mb-2 text-xl font-semibold text-neutral-50">
-        {{ title }}
-      </h5>
-      <p class="mb-4 text-base text-neutral-200">
-        {{ body }}
-      </p>
-      <div v-for="item in links" class="flex flex-col items-left">
-        <a :href="item.link" class="text-sky-500" target="_blank">
-          <Icon name="icon-park-outline:link-one" /> {{ item.label }}
-        </a>
+  <a
+    :href="link"
+    target="_blank"
+    class="inline-flex w-full flex-col justify-between transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+  >
+    <div
+      class="relative m-4 flex h-40 flex-col justify-between rounded-lg bg-slate-800 p-4"
+    >
+      <div class="z-10">
+        <h3 class="font-bold mb-2 text-lg">{{ title }}</h3>
+        <div class="my-3 line-clamp-2">{{ body }}</div>
       </div>
-      <p class="text-xs text-neutral-300 pt-4">
-        {{ timeline }}
-      </p>
+      <div class="flex items-center">
+        <svg width="20" height="20" class="mr-1">
+          <circle cx="10" cy="10" r="7" v-if="color" :class="color"></circle>
+        </svg>
+        <span>{{ lang }}</span>
+      </div>
+      <div class="absolute right-2 text-8xl backdrop-blur">
+        <Icon
+          :name="icon"
+          :class="[icon === 'iconoir:github' ? 'blur-sm' : '']"
+        />
+      </div>
     </div>
-  </div>
+  </a>
 </template>
