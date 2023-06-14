eb04dd0a9f34a0f7bb8305325595cf52255ad887
David Saah <dasorange.hope@gmail.com>
2023-06-14 15:47:00 +0000

    set copyright date to current year

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,7 +3,8 @@
     <hr
       class="my-12 h-1 border-0 bg-transparent bg-gradient-to-r from-transparent via-sky-950 to-transparent"
     />
-    &copy 2023 DaveSaah <br />
-    Powered by Nuxt <Icon name="logos:nuxt-icon" /> + Vue <Icon name="logos:vue" /> + Tailwind <Icon name="logos:tailwindcss-icon" />
+    &copy {{ new Date().getFullYear() }} DaveSaah <br />
+    Powered by Nuxt <Icon name="logos:nuxt-icon" /> + Vue
+    <Icon name="logos:vue" /> + Tailwind <Icon name="logos:tailwindcss-icon" />
   </div>
 </template>
